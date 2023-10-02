e05621e22775650d04443b9627111ac5cfcc0b3b
marciocamello <mac3designer@gmail.com>
2023-10-02 22:18:40 +0100

    Attribute accessors

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -7,6 +7,9 @@
 UAuraAttributeSet::UAuraAttributeSet()
 {
 	InitHealth(100.f);
+	InitMaxHealth(100.f);
+	InitMana(50.f);
+	InitMaxMana(50.f);
 }
 
 void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
