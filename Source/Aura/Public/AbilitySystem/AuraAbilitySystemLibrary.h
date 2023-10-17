cdeff59133a3294d9245de27ee941135274bcad5
marciocamello <mac3designer@gmail.com>
2023-10-17 21:34:31 +0100

    Constructing the Attribute Menu Widget Controller

--- Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -4,8 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
-#include "UI/WidgetController/OverlayWidgetController.h"
 #include "AuraAbilitySystemLibrary.generated.h"
+
+class UAttributeMenuWidgetController;
+class UOverlayWidgetController;
+
 
 /**
  * 
@@ -19,4 +22,7 @@ public:
 
 	UFUNCTION(BlueprintPure, Category= "Aura Ability System Library|Widget Controller")
 	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintPure, Category= "Aura Ability System Library|Widget Controller")
+	static UAttributeMenuWidgetController* GetAttributeMenuWidgetController(const UObject* WorldContextObject);
 };
