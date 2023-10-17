cdfa9bf5da9d1db548d7e98a4e92eca10f4f1fe7
marciocamello <mac3designer@gmail.com>
2023-10-17 17:46:21 +0100

    Aura Asset Manager

--- Source/Aura/Public/AuraGameplayTags.h
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -15,6 +15,8 @@ struct FAuraGameplayTags
 public:
     static const FAuraGameplayTags& Get(){  return GameplayTags; }
 	static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Armor;
     
 protected:
 	
