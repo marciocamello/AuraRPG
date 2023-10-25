245d0a26d0872f451743ed2313704bbc532b8db2
marciocamello <mac3designer@gmail.com>
2023-10-25 22:16:20 +0100

    Attack Behavior Tree Task

---
+++ Source/Aura/Private/AI/BTTask_Attack.cpp
@@ -0,0 +1,9 @@
+// Copyright Axchemy Games
+
+
+#include "AI/BTTask_Attack.h"
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	return Super::ExecuteTask(OwnerComp, NodeMemory);
+}
