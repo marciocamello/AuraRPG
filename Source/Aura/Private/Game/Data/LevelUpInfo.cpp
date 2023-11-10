034aebbf1e208267d25df5149c65312215d2bcec
marciocamello <mac3designer@gmail.com>
2023-11-10 22:56:16 +0000

    Level Up Info Data Asset

---
+++ Source/Aura/Private/Game/Data/LevelUpInfo.cpp
@@ -0,0 +1,26 @@
+﻿// Copyright Axchemy Games
+
+
+#include "Game/Data/LevelUpInfo.h"
+
+int32 ULevelUpInfo::FindLevelForXP(int32 XP)
+{
+	int32 Level = 1;
+	bool bSearching = true;
+	while(bSearching)
+	{
+		// LevelUpInformation[1] = Level 1 information
+		// LevelUpInformation[2] = Level 2 information
+		if(LevelUpInformation.Num() -1 <= Level) return Level;
+
+		if(XP >= LevelUpInformation[Level].LevelUpRequirement)
+		{
+			++Level;
+		}
+		else
+		{
+			bSearching = false;
+		}
+	}
+	return Level;
+}
