ba20dcad24a42efd495ad82b166acfdc3b4c821b
marciocamello <mac3designer@gmail.com>
2024-01-05 21:57:28 +0000

    Target Trace Channel

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -6,3 +6,5 @@
 
 #define CUSTOM_DEPTH_RED 250.f
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
+#define ECC_FadeActor ECollisionChannel::ECC_GameTraceChannel2
+#define ECC_Target ECollisionChannel::ECC_GameTraceChannel3
--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -4,6 +4,7 @@
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
 #include "AbilitySystemComponent.h"
+#include "Aura/Aura.h"
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -37,7 +38,7 @@ void UTargetDataUnderMouse::SendMouseCursorData()
 	
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorHit;
-	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	PC->GetHitResultUnderCursor(ECC_Target, false, CursorHit);
 
 	FGameplayAbilityTargetDataHandle DataHandle;
 	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
