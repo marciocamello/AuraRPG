92a81340db48ab6a0cd1d67d83999f137a66b957
Marcio Vale <marciocamello@outlook.com>
2025-09-19 21:49:53 +0100

    Documentation the project, and start a arcane shards

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,28 @@
+// Copyright Axchemy Games
+
+
+#include "Actor/MagicCircle.h"
+
+#include "Components/DecalComponent.h"
+
+AMagicCircle::AMagicCircle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>(TEXT("MagicCircleDecal"));
+	MagicCircleDecal->SetupAttachment(GetRootComponent());
+
+}
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
